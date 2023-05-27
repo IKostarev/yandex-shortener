566808a4b04fa159a1b37dd25b6c3a49fa94d5ee
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-05-27 13:13:16 +0500

    iter13: refactor try fix, step three

--- internal/handlers/batch.go
+++ internal/handlers/batch.go
@@ -28,56 +28,25 @@ func (a *App) BatchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, item := range req {
-		var r URLsResponse
 
-		short, _ := a.Storage.CheckIsURLExists(item.OriginalURL) //TODO handle error
-		if short != "" {
-			r.CorrelationID = item.CorrelationID
-			r.ShortURL, _ = url.JoinPath(a.Config.BaseShortURL, short) //TODO handle error
-			//if err != nil {
-			//	logger.Errorf("join path has error: %s", err)
-			//	w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
-			//	return
-			//}
-
-			resp = append(resp, r)
-			w.WriteHeader(http.StatusConflict)
-		} else {
-
-			//if err == nil {
-			//	if short != "" {
-			//		var r URLsResponse
-			//		r.CorrelationID = item.CorrelationID
-			//		r.ShortURL, err = url.JoinPath(a.Config.BaseShortURL, short)
-			//		if err != nil {
-			//			logger.Errorf("join path has error: %s", err)
-			//			w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
-			//			return
-			//		}
-			//
-			//		resp = append(resp, r)
-			//	}
-			//} else {
-			//	logger.Errorf("batch CheckIsURLExists is error: %s", err)
-			//}
-
-			short, err := a.Storage.Save(item.OriginalURL, item.CorrelationID)
-			if err != nil {
-				logger.Errorf("batch save is error: %s", err)
-				w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
-				return
-			}
-
-			r.CorrelationID = item.CorrelationID
-			r.ShortURL, err = url.JoinPath(a.Config.BaseShortURL, short)
-			if err != nil {
-				logger.Errorf("join path has error: %s", err)
-				w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
-				return
-			}
-
-			resp = append(resp, r)
+		short, err := a.Storage.Save(item.OriginalURL, item.CorrelationID)
+		if err != nil {
+			logger.Errorf("batch save is error: %s", err)
+			w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
+			return
 		}
+
+		var r URLsResponse
+		r.CorrelationID = item.CorrelationID
+		r.ShortURL, err = url.JoinPath(a.Config.BaseShortURL, short)
+		if err != nil {
+			logger.Errorf("join path has error: %s", err)
+			w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
+			return
+		}
+
+		// действительно не нужно, уже не помню почему оно тут вообще есть  _, r.CorrelationID = a.Storage.Get(r.ShortURL, item.CorrelationID)
+		resp = append(resp, r)
 	}
 
 	respContent, err := json.Marshal(resp)
@@ -93,3 +62,99 @@ func (a *App) BatchHandler(w http.ResponseWriter, r *http.Request) {
 		logger.Errorf("Failed to send URLsResponse on batch handler: %s", err)
 	}
 }
+
+//package handlers
+//
+//import (
+//	"encoding/json"
+//	"github.com/IKostarev/yandex-go-dev/internal/logger"
+//	"net/http"
+//	"net/url"
+//)
+//
+//type URLsRequest struct {
+//	CorrelationID string `json:"correlation_id"`
+//	OriginalURL   string `json:"original_url"`
+//}
+//
+//type URLsResponse struct {
+//	CorrelationID string `json:"correlation_id"`
+//	ShortURL      string `json:"short_url"`
+//}
+//
+//func (a *App) BatchHandler(w http.ResponseWriter, r *http.Request) {
+//	var req []URLsRequest
+//	var resp []URLsResponse
+//
+//	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+//		logger.Errorf("json decode is error: %s", err)
+//		w.WriteHeader(http.StatusBadRequest)
+//		return
+//	}
+//
+//	for _, item := range req {
+//		var r URLsResponse
+//
+//		short, _ := a.Storage.CheckIsURLExists(item.OriginalURL) //TODO handle error
+//		if short != "" {
+//			r.CorrelationID = item.CorrelationID
+//			r.ShortURL, _ = url.JoinPath(a.Config.BaseShortURL, short) //TODO handle error
+//			//if err != nil {
+//			//	logger.Errorf("join path has error: %s", err)
+//			//	w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
+//			//	return
+//			//}
+//
+//			resp = append(resp, r)
+//			w.WriteHeader(http.StatusConflict)
+//		} else {
+//
+//			//if err == nil {
+//			//	if short != "" {
+//			//		var r URLsResponse
+//			//		r.CorrelationID = item.CorrelationID
+//			//		r.ShortURL, err = url.JoinPath(a.Config.BaseShortURL, short)
+//			//		if err != nil {
+//			//			logger.Errorf("join path has error: %s", err)
+//			//			w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
+//			//			return
+//			//		}
+//			//
+//			//		resp = append(resp, r)
+//			//	}
+//			//} else {
+//			//	logger.Errorf("batch CheckIsURLExists is error: %s", err)
+//			//}
+//
+//			short, err := a.Storage.Save(item.OriginalURL, item.CorrelationID)
+//			if err != nil {
+//				logger.Errorf("batch save is error: %s", err)
+//				w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
+//				return
+//			}
+//
+//			r.CorrelationID = item.CorrelationID
+//			r.ShortURL, err = url.JoinPath(a.Config.BaseShortURL, short)
+//			if err != nil {
+//				logger.Errorf("join path has error: %s", err)
+//				w.WriteHeader(http.StatusBadRequest) // TODO: в будущем переделать на http.StatusInternalServerError
+//				return
+//			}
+//
+//			resp = append(resp, r)
+//		}
+//	}
+//
+//	respContent, err := json.Marshal(resp)
+//	if err != nil {
+//		logger.Errorf("json marshal is error: %s", err)
+//		w.WriteHeader(http.StatusBadRequest)
+//		return
+//	}
+//
+//	w.Header().Set("Content-Type", "application/json")
+//	w.WriteHeader(http.StatusCreated)
+//	if _, err := w.Write(respContent); err != nil {
+//		logger.Errorf("Failed to send URLsResponse on batch handler: %s", err)
+//	}
+//}
