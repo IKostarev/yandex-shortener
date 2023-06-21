4fcb50aac5a9ec3b9e9a5d4aee122fe963e625ba
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-06-21 22:56:20 +0500

    iter14: refactor - try add cookie

--- internal/middleware/auth/auth.go
+++ internal/middleware/auth/auth.go
@@ -2,13 +2,38 @@ package auth
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"net/http"
+	"time"
 )
 
 func Cookie(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cookie, _ := r.Cookie("ID")
-		fmt.Println("Cookie = ", cookie)
+		if cookie == nil {
+			fmt.Println("Cookie = ", cookie)
+
+			fmt.Println("createNewUser(w) = ", createNewUser(w))
+
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		fmt.Println("Cookie NOT NIL = ", cookie)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func createNewUser(w http.ResponseWriter) string {
+	user := uuid.New().String()
+
+	newCookie := http.Cookie{
+		Name:    "ID",
+		Value:   user,
+		Expires: time.Now().Add(365 * 24 * time.Hour),
+	}
+
+	http.SetCookie(w, &newCookie)
+
+	return user
 }
